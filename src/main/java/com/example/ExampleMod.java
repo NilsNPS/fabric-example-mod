136f76078b0153955be8af01177d7d4488b8f7e4
NilsNPS <nilswnps@gmail.com>
2026-08-23 17:58:01 +0200

    M

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,10 +1,15 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
+    public static final String MOD_ID = "modid";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-        // Bleibt leer für Client-Mods
+        LOGGER.info("Hardcore Heart Mod geladen!");
     }
 }
